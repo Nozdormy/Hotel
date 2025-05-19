be62527a8c960e4693bdc9f3ead2fe7e6c63e101
Gennadiy <shindyapingena@gmail.com>
2025-05-19 16:44:17 +0400

    repository updates

--- src/main/java/repository/RoomRepository.java
+++ src/main/java/repository/RoomRepository.java
@@ -3,16 +3,16 @@ package repository;
 import model.Room;
 
 import java.time.LocalDate;
-import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface RoomRepository {
 
     Optional<Room> findById(int roomId);
 
-    Collection<Room> findAll();
+    List<Room> findAll();
 
-    Collection<LocalDate> findDatesByRoomId(int roomId);
+    List<LocalDate> findDatesByRoomId(int roomId);
 
-    void saveNewReservation(int roomId, Collection<LocalDate> dates);
+    void saveNewReservation(int roomId, List<LocalDate> dates);
 }
--- src/main/java/repository/RoomRepositoryImpl.java
+++ src/main/java/repository/RoomRepositoryImpl.java
@@ -3,15 +3,13 @@ package repository;
 import model.Room;
 
 import java.time.LocalDate;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public class RoomRepositoryImpl implements RoomRepository {
 
-    /*
-        //TODO: реализовать хранение состояния комнат и бронирований
-    */
+    //TODO: реализовать хранение состояния комнат и бронирований
+    //TODO: реализовать инициализацию состояния
 
     @Override
     public Optional<Room> findById(int roomId) {
@@ -20,19 +18,19 @@ public class RoomRepositoryImpl implements RoomRepository {
     }
 
     @Override
-    public Collection<Room> findAll() {
+    public List<Room> findAll() {
         //TODO: реализовать логику поиска всех комнат
         return List.of();
     }
 
     @Override
-    public Collection<LocalDate> findDatesByRoomId(int roomId) {
+    public List<LocalDate> findDatesByRoomId(int roomId) {
         //TODO: реализовать логику поиска бронирований по roomId
         return List.of();
     }
 
     @Override
-    public void saveNewReservation(int roomId, Collection<LocalDate> dates) {
+    public void saveNewReservation(int roomId, List<LocalDate> dates) {
         //TODO: реализовать логику создания новых бронирований
     }
 }
